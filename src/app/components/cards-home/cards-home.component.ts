03fa303242888615d024ef0b6b9f26fc2a36b13f
João Henrique <joaohnsousa@gmail.com>
2024-04-06 00:54:58 -0300

    Data card

--- src/app/components/cards-home/cards-home.component.ts
+++ src/app/components/cards-home/cards-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards-home',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards-home.component.css']
 })
 export class CardsHomeComponent implements OnInit {
-
+  @Input()title: String = '';
+  @Input()src: String = '';
   constructor() { }
 
   ngOnInit(): void {
